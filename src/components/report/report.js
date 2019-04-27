60bd4875f141e4f58335096b63acc0ccb71890c4
Stephen Chung <me@stephenchung.io>
2019-04-27 13:35:00 -0400

    Continue refacotring

---
+++ src/components/report/report.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+class Report extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+     <h1>
+       report
+     </h1>
+    )
+  }
+}
+
+const msp = (state) => {
+  return {
+  }
+}
+
+const mdp = (dispatch) => {
+  return {
+  }
+}
+
+export default connect(msp, mdp)(Report);
