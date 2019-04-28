25ce8df772e0ade8534c3bc10a6b960154f02491
agronv <avelaj96@gmail.com>
2019-04-27 23:14:46 -0400

    style chart

--- src/components/report/chart.js
+++ src/components/report/chart.js
@@ -50,7 +50,8 @@ class Chart extends Component {
             tickLine={false}
             tick={{ fontSize: 15, fontWeight: 700, fill: '#FFF' }}
             ticks={ticks} axisLine={false} tickMargin={10} />
-          <Tooltip contentStyle={{ backgroundColor: '#B41F85', border: '0', borderRadius: "5px" }} />
+          <Tooltip contentStyle={{ backgroundColor: '#B41F85', border: '0', borderRadius: "5px" }} 
+            cursor={{ stroke: '#B41F85', strokeWidth: 2 }} />
           {line1}
           {line2}
         </LineChart>
